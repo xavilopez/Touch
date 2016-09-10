202e442556d54dbc23db9e3734c8eecdd77fe8e4
xavilopez <xavi_lopez011@hotmail.com>
2016-09-10 12:42:45 -0400

    Added Touch
    Added Touch

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import {AppRegistry} from 'react-native';
+
+//import PressDemo from './PressDemo';
+import Modal from './Modal';
+AppRegistry.registerComponent('Touch', () => Modal);
